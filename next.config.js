a9590cdd00ece35fe09cb7c5df9163845a7d9cf0
syedhabib <syedmirhabib@gmail.com>
2025-06-03 23:20:07 +0600

    ,,,

--- next.config.js
+++ next.config.js
@@ -13,6 +13,17 @@ const nextConfig = {
       },
     ],
   },
+  output: 'standalone',
+  experimental: {
+    optimizePackageImports: ['lucide-react'],
+  },
+  webpack: (config) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig 
